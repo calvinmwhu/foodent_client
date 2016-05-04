21bc80baf400a105d16dc83dee1243ec64a2e082
Mingwei Hu <calvinmwhu@gmail.com>
2016-05-03 23:16:26 -0500

    s

--- js/controllers.js
+++ js/controllers.js
@@ -5,8 +5,8 @@ var foodentControllers = angular.module('foodentControllers', []);
 
 foodentControllers.controller('HomeController', ['$scope', function ($scope) {
     /*
-    The following lines control the event grid 
-    */
+     The following lines control the event grid
+     */
     var $grid = $('.grid');
     $grid.masonry({
         itemSelector: '.grid-item',
@@ -22,13 +22,21 @@ foodentControllers.controller('HomeController', ['$scope', function ($scope) {
 
     });
     /*-------------------------------------
-    */
-    
+     */
+
     /* Add service calls below */
 }]);
 
+//NavBarController
+foodentControllers.controller('NavBarController', ['$scope', '$location', 'AuthService', function ($scope, $location, AuthService) {
+    $scope.logout = function () {
+        AuthService.logout();
+        $location.path('/home');
+    };
+}]);
+
 foodentControllers.controller('UserController', ['$scope', function ($scope) {
-$scope.name = "Anurag" //test 
+    $scope.name = "Anurag" //test
     var $grid = $('.grid');
 
     $grid.masonry({
@@ -53,7 +61,117 @@ foodentControllers.controller('EventController', ['$scope', function ($scope) {
 foodentControllers.controller('AddEventController', ['$scope', function ($scope) {
 
 }]);
-foodentControllers.controller('SignUpController', ['$scope', function ($scope) {
+
+foodentControllers.controller('SignUpController', ['$scope', '$location', 'AuthService', 'DEFAULT_IMAGES', function ($scope, $location, AuthService, DEFAULT_IMAGES) {
+    if (AuthService.isAuthenticated()) {
+        $location.path('/userprofile');
+    } else {
+        $scope.user = {email: '', password: '', about: ''};
+        $scope.signup = function () {
+            if (!$scope.user.profileImage) {
+                //DEFAULT_IMAGES.urls
+                var idx = Math.floor(Math.random() * DEFAULT_IMAGES.urls.length);
+                $scope.user.profileImage = DEFAULT_IMAGES.urls[idx];
+            }
+            console.log($scope.user);
+            AuthService.signup($scope.user).then(function (response) {
+                console.log(response.data);
+                $location.path('/userprofile');
+            }, function (response) {
+                console.log(response.data);
+            });
+        };
+    }
+}]);
+
+foodentControllers.controller('LoginController', ['$scope', '$location', 'AuthService', function ($scope, $location, AuthService) {
+    if (AuthService.isAuthenticated()) {
+        $location.path('/userprofile');
+    } else {
+        $scope.user = {email: '', password: ''};
+        $scope.login = function () {
+            //AuthService.
+            AuthService.login($scope.user).then(function (response) {
+                console.log(response.data);
+                $location.path('/userprofile');
+            }, function (response) {
+                console.log(response.data);
+            });
+        };
+    }
 
 }]);
+
+
+foodentControllers.controller('UserProfileController', ['$scope', '$location', 'UserService', 'AuthService', function ($scope, $location, UserService, AuthService) {
+
+    if (!AuthService.isAuthenticated()) {
+        $location.path('/login');
+    } else {
+        $scope.user = {};
+        UserService.getUserDetail().then(function (response) {
+            console.log(response.data);
+            $scope.user = response.data.data;
+        }, function (response) {
+            $scope.response = response;
+            console.log(response.data);
+        });
+    }
+    $scope.getFollowers = function () {
+        var queryParams = {
+            where: {
+                _id: {
+                    $in: $scope.user.followers
+                }
+            },
+            select: {
+                name: 1,
+                _id: 1,
+                profileImage: 1
+            },
+            sort: {
+                name: -1
+            },
+            skip: 0
+        };
+        UserService.getUsers(queryParams).then(function (response) {
+            $scope.followers = response.data.data;
+        }, function (response) {
+            console.log(response);
+        });
+    };
+
+    $scope.getFollowings = function () {
+        var queryParams = {
+            where: {
+                _id: {
+                    $in: $scope.user.following
+                }
+            },
+            select: {
+                name: 1,
+                _id: 1,
+                profileImage: 1
+            },
+            sort: {
+                name: -1
+            },
+            skip: 0
+        };
+        UserService.getUsers(queryParams).then(function (response) {
+            $scope.following = response.data.data;
+        }, function (response) {
+            console.log(response);
+        });
+    };
+
+    //
+    //$scope.logout = function () {
+    //    AuthService.logout();
+    //};
+
+}]);
+
+
+
 
--- js/services.js
+++ js/services.js
@@ -3,23 +3,112 @@
  */
 var foodentServices = angular.module('foodentServices', []);
 
-foodentServices.factory('CommonData', function(){
-    var data = "";
-    return{
-        getData : function(){
-            return data;
-        },
-        setData : function(newData){
-            data = newData;
-        }
-    }
-});
 
-foodentServices.factory('Llamas', function($http, $window) {
-    return {
-        get : function() {
-            var baseUrl = $window.sessionStorage.baseurl;
-            return $http.get(baseUrl+'/api/llamas');
+foodentServices.factory('AuthService', ['$http', '$q', 'API_ENDPOINT', function ($http, $q, API_ENDPOINT) {
+    var ACCESS_TOKEN = 'access_token';
+    var isAuthenticated = false;
+    var authToken;
+
+    function useCredentials(token) {
+        isAuthenticated = true;
+        authToken = token;
+        // Set the token as header for your requests!
+        $http.defaults.headers.common.Authorization = authToken;
+    }
+
+    function loadUserCredentials() {
+        var token = window.localStorage.getItem(ACCESS_TOKEN);
+        if (token) {
+            useCredentials(token);
         }
     }
-});
+
+    function storeUserCredentials(token) {
+        window.localStorage.setItem(ACCESS_TOKEN, token);
+        useCredentials(token);
+    }
+
+    var signup = function (user) {
+        var config = {
+            method: 'POST',
+            url: API_ENDPOINT.url + '/signup',
+            data: user
+        };
+        return $q(function (resolve, reject) {
+            $http(config).then(function (response) {
+                storeUserCredentials(response.data.token);
+                resolve(response);
+            }, function (response) {
+                reject(response);
+            });
+        });
+    };
+
+    var login = function (user) {
+        var config = {
+            method: 'POST',
+            url: API_ENDPOINT.url + '/authenticate',
+            data: user
+        };
+        return $q(function (resolve, reject) {
+            $http(config).then(function (response) {
+                storeUserCredentials(response.data.token);
+                resolve(response);
+            }, function (response) {
+                console.log(response);
+                reject(response);
+            });
+        });
+    };
+
+    function destroyUserCredentials() {
+        authToken = undefined;
+        isAuthenticated = false;
+        $http.defaults.headers.common.Authorization = undefined;
+        window.localStorage.removeItem(ACCESS_TOKEN);
+        console.log(window.localStorage);
+    }
+
+    var logout = function () {
+
+        destroyUserCredentials();
+    };
+
+    loadUserCredentials();
+
+    return {
+        login: login,
+        signup: signup,
+        logout: logout,
+        isAuthenticated: function () {
+            return isAuthenticated;
+        }
+    }
+
+}]);
+
+foodentServices.factory('UserService', ['$http', '$q', 'API_ENDPOINT', function ($http, $q, API_ENDPOINT) {
+    var getUserDetail = function(queryParams){
+        var config = {
+            method: 'GET',
+            url: API_ENDPOINT.url + '/userprofile',
+            params: queryParams
+        };
+        return $http(config);
+    };
+
+    var getUsers = function(queryParams){
+        var config = {
+            method: 'GET',
+            url: API_ENDPOINT.url + '/users',
+            params: queryParams
+        };
+        return $http(config);
+    };
+
+
+    return {
+        getUserDetail: getUserDetail,
+        getUsers: getUsers
+    };
+}]);
